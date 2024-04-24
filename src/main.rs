0bde8c334d51065ea1d08b9531a61734d83ebeb5
klx01 <69247896+klx01@users.noreply.github.com>
2024-04-24 19:19:39 +0400

    implement decode for strings

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,35 @@
+use anyhow::{bail, Context};
 use serde_json;
-use std::env;
+use clap::{Parser, Subcommand};
 
-// Available if you need it!
-// use serde_bencode
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
 
-#[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
-        // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
+#[derive(Subcommand)]
+enum Command {
+    Decode {
+        value: String,
     }
 }
 
-// Usage: your_bittorrent.sh decode "<encoded_value>"
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let command = &args[1];
-
-    if command == "decode" {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        println!("Logs from your program will appear here!");
-
-        // Uncomment this block to pass the first stage
-        // let encoded_value = &args[2];
-        // let decoded_value = decode_bencoded_value(encoded_value);
-        // println!("{}", decoded_value.to_string());
-    } else {
-        println!("unknown command: {}", args[1])
+fn main() -> anyhow::Result<()> {
+    let cli = Cli::parse();
+    match cli.command {
+        Command::Decode { value } => decode_value(value),
     }
+}
+
+fn decode_value(value: String) -> anyhow::Result<()> {
+    let (length, string) = value.split_once(':').context("delimiter not found")?;
+    let length = length.parse::<usize>().context("length is not a valid number")?;
+    let actual_len = string.len();
+    if actual_len < length {
+        bail!("actual len {actual_len} is smaller than expected {length}");
+    }
+    let string = &string[..length];
+    println!("\"{string}\"");
+    Ok(())
 }
