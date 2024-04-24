cc2ed503e5fe1f5bde5ea946ff18a1d191eb4c7b
klx01 <69247896+klx01@users.noreply.github.com>
2024-04-24 19:20:10 +0400

    implement decode for integers

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use anyhow::{bail, Context};
-use serde_json;
 use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
@@ -23,6 +22,17 @@ fn main() -> anyhow::Result<()> {
 }
 
 fn decode_value(value: String) -> anyhow::Result<()> {
+    let Some(first) = value.chars().next() else {
+        bail!("empty input");
+    };
+    match first {
+        'i' => decode_int(value),
+        '0'..='9' => decode_string(value),
+        _ => bail!("invalid format, can't parse a value that starts with {first}"),
+    }
+}
+
+fn decode_string(value: String) -> anyhow::Result<()> {
     let (length, string) = value.split_once(':').context("delimiter not found")?;
     let length = length.parse::<usize>().context("length is not a valid number")?;
     let actual_len = string.len();
@@ -31,5 +41,12 @@ fn decode_value(value: String) -> anyhow::Result<()> {
     }
     let string = &string[..length];
     println!("\"{string}\"");
+    Ok(())
+}
+
+fn decode_int(value: String) -> anyhow::Result<()> {
+    let (num, _) = value[1..].split_once('e').context("end of integer not found")?;
+    let num = num.parse::<i64>().context("invalid int")?;
+    println!("{num}");
     Ok(())
 }
